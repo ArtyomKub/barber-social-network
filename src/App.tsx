7215e20f4ee46f4befff9fab45f6bcff8b8d4ba8
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-23 23:33:58 +0200

    Add new div with text (Profile, Messages, News, Music, Settings)

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,28 @@ import {Footer} from './Footer';
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <header className='header'>
+        <div className="app-wrapper">
+            <header className="header">
                 <img src="https://th.bing.com/th/id/OIP.fCDkjGeHw9blnre1P9Vj9wHaHa?rs=1&pid=ImgDetMain" alt="logo"/>
             </header>
             <nav className="nav">
-                <div>Profile</div>
-                <div>Messages</div>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
             </nav>
-            <div className='content'>
+            <div className="content">
                 Main content
             </div>
 
