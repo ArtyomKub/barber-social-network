37f6f206efc707e57ea525f5d10441fffbb2139e
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-24 00:27:23 +0200

    Delete unused import in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Header} from './Header';
-import {Technologies} from './Technologies';
-import {Footer} from './Footer';
+
 
 function App() {
     return (
