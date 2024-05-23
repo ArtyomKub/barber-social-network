ff6c71c4ef7de94a7ee318624f46d0d4285b2aed
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-23 23:54:18 +0200

    Create a simple structure with div tag

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,26 @@ function App() {
                 </div>
             </nav>
             <div className="content">
-                <img
-                    src="https://static.wixstatic.com/media/b40ba8e9258a4c589b794d706d9d1e0f.jpg/v1/crop/x_0,y_214,w_4000,h_1191/fill/w_940,h_280,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Man%20Getting%20a%20Haircut.jpg"
-                    alt="content"/>
+                <div>
+                    <img
+                        src="https://static.wixstatic.com/media/b40ba8e9258a4c589b794d706d9d1e0f.jpg/v1/crop/x_0,y_214,w_4000,h_1191/fill/w_940,h_280,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Man%20Getting%20a%20Haircut.jpg"
+                        alt="content"/>
+                </div>
+                <div>
+                    ava+description
+                    <img src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/117e64133348381.61bb4e7066391.jpg" alt="portrait"/>
+                </div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>
+                        <div>Post 1</div>
+                        <div>Post 2</div>
+                        <div>Post 3</div>
+                        <div>Post 4</div>
+                    </div>
+                </div>
+
             </div>
 
 
