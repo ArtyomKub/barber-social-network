38547a128b293840cc7a8bee0f4da6c17e46c6c6
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-24 00:40:32 +0200

    Remove error in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,8 @@ import React from 'react';
 
 export const Header = () => {
     return (
-        <div>
-            <header className="header">
-                <img src="https://th.bing.com/th/id/OIP.fCDkjGeHw9blnre1P9Vj9wHaHa?rs=1&pid=ImgDetMain" alt="logo"/>
-            </header>
-        </div>
+        <header className="header">
+            <img src="https://th.bing.com/th/id/OIP.fCDkjGeHw9blnre1P9Vj9wHaHa?rs=1&pid=ImgDetMain" alt="logo"/>
+        </header>
     );
 };
