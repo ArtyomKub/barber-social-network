f703f51da4ca54982f6cf0871074c82d0898f383
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-23 22:59:56 +0200

    Add tag 'headder and nav' with some text. Comment our components

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,24 @@ import {Footer} from './Footer';
 
 function App() {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies/>
-            <Footer/>
+        <div>
+            <header>
+                <img src="https://i.pinimg.com/736x/8d/0c/79/8d0c79c25cee23c9af669d1ff9ee4246.jpg" alt="logo"/>
+            </header>
+            <nav>
+                <div>Profile</div>
+                <div>Messages</div>
+            </nav>
+            <div>
+                Main content
+            </div>
+
+
+
+
+            {/*<Header/>*/}
+            {/*<Technologies/>*/}
+            {/*<Footer/>*/}
         </div>
     );
 }
