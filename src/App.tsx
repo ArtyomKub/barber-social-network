0fb0bbe30af563a917d2710ed7bd85042bf2a9b5
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-23 22:31:38 +0200

    Connect component 'Header' in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-
 import './App.css';
+import {Header} from './Header';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div className="App">
+            <Header/>
 
-
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
