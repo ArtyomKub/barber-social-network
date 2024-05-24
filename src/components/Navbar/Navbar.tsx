4a9442af4c19a3b4bb0114da6bfd1ab801a5b5d2
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-25 01:00:52 +0200

    Change text in all tag

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,19 +5,19 @@ export const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a>Profile</a>
+                <a>PROFILE</a>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a>Messages</a>
+                <a>MESSAGES</a>
             </div>
             <div className={s.item}>
-                <a>News</a>
+                <a>NEWS</a>
             </div>
             <div className={s.item}>
-                <a>Music</a>
+                <a>MUSIC</a>
             </div>
             <div className={s.item}>
-                <a>Settings</a>
+                <a>SETTINGS</a>
             </div>
         </nav>
     );
