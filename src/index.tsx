e1da8038d0f24706dac8deb0357a81751b947caa
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-24 20:33:53 +0200

    Add a perfect background video title in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -8,9 +8,12 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <video autoPlay={true} loop muted id={'video'}>
+            <source src={'https://media.istockphoto.com/id/1251555171/pl/filmy/vintage-fryzjer-wn%C4%99trze-ruch-wzd%C5%82u%C5%BC-krzese%C5%82-drewnianych-sto%C5%82%C3%B3w-i-luster-stylowe-studio-w%C5%82os%C3%B3w.mp4?s=mp4-640x640-is&k=20&c=6XbuvNh-4uNTREsK2sYXZAxAxbvPUXk2M3HD7Gej960='}/>
+        </video>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
