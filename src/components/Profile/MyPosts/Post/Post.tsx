beda4f668ae674c73b07b07b9590a452431442aa
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-25 00:44:36 +0200

    Add typing and props (message)

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import s from './Post.module.css';
 
-export const Post = () => {
+type PostType = {
+    message: string
+}
+
+export const Post = (props: PostType) => {
     return (
         <div className={s.item}>
             <img
                 src="https://www.famousbirthdays.com/faces/siriboe-kofi-image.jpg"
                 alt="avatar"/>
-            Post 1
+            {props.message}
             <div>
                 <button>Like</button>
             </div>
