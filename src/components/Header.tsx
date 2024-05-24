bd6862499baff07f7da7156c002ad3455bc490db
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-24 14:38:00 +0200

    Import Header.css in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import './Header.css';
 
 export const Header = () => {
     return (
         <header className="header">
-            <img src="https://png.pngtree.com/png-vector/20220606/ourmid/pngtree-barbershop-logo-png-image_4925346.png" alt="logo"/>
+            <img src="https://png.pngtree.com/png-vector/20220606/ourmid/pngtree-barbershop-logo-png-image_4925346.png"
+                 alt="logo"/>
         </header>
     );
 };
