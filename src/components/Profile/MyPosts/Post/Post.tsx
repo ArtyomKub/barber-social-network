ddac909476b396b1bbba67f7ad0c2cc6cc9ce72a
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-25 00:00:43 +0200

    Add two button

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -8,6 +8,9 @@ export const Post = () => {
                 src="https://e3.365dm.com/21/10/768x432/skynews-colin-kaepernick-black-and-white_5561588.jpg?20211027182700"
                 alt="avatar"/>
             Post 1
+            <div>
+                <button>Like</button>
+            </div>
         </div>
     );
 };
