9c03927e0b00219e7a63af8ff512675f008f8a9e
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-24 23:56:36 +0200

    Connect new component Post

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from './MyPosts.module.css';
+import {Post} from './Post/Post';
 
 export const MyPosts = () => {
     return (
@@ -11,15 +12,12 @@ export const MyPosts = () => {
                 <button>Delete Post</button>
             </div>
             <div className={s.posts}>
-                <div className={s.item}>
-                    <img
-                        src="https://e3.365dm.com/21/10/768x432/skynews-colin-kaepernick-black-and-white_5561588.jpg?20211027182700"
-                        alt="avatar"/>
-                    Post 1
-                </div>
-                <div className={s.item}>Post 2</div>
-                <div className={s.item}>Post 3</div>
-                <div className={s.item}>Post 4</div>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
+                <Post/>
             </div>
         </div>
     );
