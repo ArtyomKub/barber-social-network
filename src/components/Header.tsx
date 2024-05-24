b5c617003295d250e77ada1080efbec55db8fb93
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-24 15:01:20 +0200

    Add classes for tag 'header' in Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import s from './Header.module.css';
 
 export const Header = () => {
     return (
-        <header className="header">
+        <header className={s.header}>
             <img src="https://png.pngtree.com/png-vector/20220606/ourmid/pngtree-barbershop-logo-png-image_4925346.png"
                  alt="logo"/>
         </header>
