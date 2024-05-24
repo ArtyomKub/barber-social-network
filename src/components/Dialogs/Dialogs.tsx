c680d7ea295b9ea79be2dad779d4686da5e1e758
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-25 01:11:18 +0200

    Delete unused style

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import s from './Dialogs.module.css'
+
 
 export const Dialogs = () => {
     return (
-        <div className={s.item}>
+        <div>
             DIALOGS
         </div>
     );
