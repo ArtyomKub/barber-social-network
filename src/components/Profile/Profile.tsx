f8eabbde28c6fc5e8a8d6877de5839bbc037f7ba
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-25 01:11:09 +0200

    Delete unused style

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -4,7 +4,7 @@ import {MyPosts} from './MyPosts/MyPosts';
 
 export const Profile = () => {
     return (
-        <div className={s.content}>
+        <div>
             <div>
                 <img
                     src="https://static.wixstatic.com/media/b40ba8e9258a4c589b794d706d9d1e0f.jpg/v1/crop/x_0,y_214,w_4000,h_1191/fill/w_940,h_280,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Man%20Getting%20a%20Haircut.jpg"
