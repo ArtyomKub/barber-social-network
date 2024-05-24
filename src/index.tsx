d8755da47dc06082e9d6ef4608459e7ec514e7ce
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-24 23:05:42 +0200

    Connect new components VideoBackground in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {VideoBackground} from './components/VideoBackground';
+
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <video autoPlay={true} loop muted id={'video'} className='cover'>
-            <source src={'https://media.istockphoto.com/id/1251555171/pl/filmy/vintage-fryzjer-wn%C4%99trze-ruch-wzd%C5%82u%C5%BC-krzese%C5%82-drewnianych-sto%C5%82%C3%B3w-i-luster-stylowe-studio-w%C5%82os%C3%B3w.mp4?s=mp4-640x640-is&k=20&c=6XbuvNh-4uNTREsK2sYXZAxAxbvPUXk2M3HD7Gej960='}/>
-        </video>
+        <VideoBackground/>
         <App/>
     </React.StrictMode>
 );
