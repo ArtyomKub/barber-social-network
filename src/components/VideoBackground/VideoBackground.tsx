4577b85816e4a3b57b92cd9b50be39818c244451
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-24 23:21:13 +0200

    import module-css (s) in this component

--- src/components/VideoBackground/VideoBackground.tsx
+++ src/components/VideoBackground/VideoBackground.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import './VideoBackground.css'
+import s from './VideoBackground.module.css'
 
 export const VideoBackground = () => {
     return (
-        <video autoPlay={true} loop muted id={'video'} className="cover">
+        <video autoPlay={true} loop muted id={s.video} className={s.cover}>
             <source
                 src={'https://media.istockphoto.com/id/1251555171/pl/filmy/vintage-fryzjer-wn%C4%99trze-ruch-wzd%C5%82u%C5%BC-krzese%C5%82-drewnianych-sto%C5%82%C3%B3w-i-luster-stylowe-studio-w%C5%82os%C3%B3w.mp4?s=mp4-640x640-is&k=20&c=6XbuvNh-4uNTREsK2sYXZAxAxbvPUXk2M3HD7Gej960='}/>
         </video>
