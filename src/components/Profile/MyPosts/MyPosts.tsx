96c58371c3418df75a575958d5a812e3c86951bd
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-25 00:50:00 +0200

    Add likesCount in component Post

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -14,10 +14,10 @@ export const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message = 'Hi! I am looking a good Barber' likesCount={10}/>
-                <Post message = 'What a FUCK!'likesCount={3}/>
-                <Post message = 'Ulyana is the Best Barber' likesCount={51}/>
-                <Post message = 'No, the best Barber is Maximus' likesCount={12}/>
+                <Post message="Hi! I am looking a good Barber" likesCount={10}/>
+                <Post message="What a FUCK!" likesCount={3}/>
+                <Post message="Ulyana is the Best Barber" likesCount={51}/>
+                <Post message="No, the best Barber is Maximus" likesCount={12}/>
             </div>
         </div>
     );
