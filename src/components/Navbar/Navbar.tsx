a14234ab85c5eff4674b9dc58a9f1b02d4328795
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-25 02:10:09 +0200

    Add active link style for all links in Navbar components

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,19 +6,19 @@ export const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <NavLink to="/profile">PROFILE</NavLink>
-            </div>
-            <div className={`${s.item} ${s.active}`}>
-                <NavLink to="/dialogs">MESSAGES</NavLink>
+                <NavLink to="/profile" className={navData => navData.isActive ? s.active : s.item}>PROFILE</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/news">NEWS</NavLink>
+                <NavLink to="/dialogs" className={navData => navData.isActive ? s.active : s.item}>MESSAGES</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/music">MUSIC</NavLink>
+                <NavLink to="/news" className={navData => navData.isActive ? s.active : s.item}>NEWS</NavLink>
             </div>
             <div className={s.item}>
-                <NavLink to="/settings">SETTINGS</NavLink>
+                <NavLink to="/music" className={navData => navData.isActive ? s.active : s.item}>MUSIC</NavLink>
+            </div>
+            <div className={s.item}>
+                <NavLink to="/settings" className={navData => navData.isActive ? s.active : s.item}>SETTINGS</NavLink>
             </div>
         </nav>
     );
