410e095258db0d0fc917bfe7d830cba59b485abf
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-25 15:50:10 +0200

    ADd new structure for dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,29 @@ import React from 'react';
 
 export const Dialogs = () => {
     return (
-        <div>
-            DIALOGS
+        <div className='dialogs'>
+            <div className='dialogs-items'>
+                <div className='dialog'>
+                    Artyom
+                </div>
+                <div className='dialog'>
+                    Uliana
+                </div>
+                <div className='dialog'>
+                    Teona
+                </div>
+                <div className='dialog'>
+                    Sofia
+                </div>
+                <div className='dialog'>
+                    Natalia
+                </div>
+            </div>
+            <div className='mesages'>
+                <div className="message">Hi</div>
+                <div className="message">How are you</div>
+                <div className="message">I am GOD!</div>
+            </div>
         </div>
     );
 };
